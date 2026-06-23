b615447d4845f088e6a3ca1a68d6b04c374a7d62
Mustafa Abozaina <MustafaAbozaina@gmail.com>
2026-06-24 01:52:20 +0300

    Initial Commit

---
+++ FeasoTests/FeasoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FeasoTests.swift
+//  FeasoTests
+//
+//  Created by Mustafa Abozaina on 6/24/26.
+//
+
+import Testing
+@testable import Feaso
+
+struct FeasoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
